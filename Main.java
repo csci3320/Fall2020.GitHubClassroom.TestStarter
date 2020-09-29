945faff9a1ef08cf8ce3082ffe22f344f1be6602
B. Ricks <bricks@unomaha.edu>
2020-09-29 11:57:21 -0500

    Initial Commit

---
+++ Main.java
@@ -0,0 +1,23 @@
+public class Main{
+  public static void main(String[] args) {
+    new Main();
+  }
+
+  public Main(){
+    /**
+     * This is my first test using GitHub class room.
+     * It might be a colossal failure.
+     * But it's worth a try.
+     * 
+     * If your up for an adventure, take this starter
+     * code and change it so it prints out the
+     * array in order, but with only recursion.
+     * Don't use a for/while/ or do-while loop.
+     * 
+     * You'll obviously need to add a new method 
+     * to this Main class.
+     */
+
+     String[] toPrint = new String[]{"Alice", "Bob", "Eve"};
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Fall2020.GitHubClassroom.TestStarter
+
+This is my first test using GitHub class room.
+It might be a colossal failure.
+But it's worth a try.
+     * 
+If your up for an adventure, take this starter
+code and change it so it prints out the
+array in order, but with only recursion.
+Don't use a for/while/ or do-while loop.
+     * 
+You'll obviously need to add a new method 
+to this Main class.
